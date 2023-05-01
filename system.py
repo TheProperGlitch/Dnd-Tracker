fe2d1b6bfe451aeeba06953716b362c2b88c1adb
TheProperGlitch <114691366+TheProperGlitch@users.noreply.github.com>
2023-05-01 13:02:21 -0700

    Started re-working saving

--- system.py
+++ system.py
@@ -1,33 +1,34 @@
 import os
 import json
-
+os.chdir("People")
 #Editting a creature:
 def c_edit():
     print("")
 
 #Making a new creature:
 def c_make():
-    creature = {}
+    creature = []
     creature_list = []
     print("What would you like to name the creature? (The name must be lowercase and have no spaces.) \n(Example: bobby)")
-    creature["name"] = input("")
+    creature.append(input(""))
     print("How many points of health would you like to give this creature? \n(The health must be a full integer made with number symbols) \n(Example: 592)")
-    creature["health"] = int(input(""))
+    creature.append(int(input(""))) 
     print("What is this creature's order in the initative. (The initative must be a full integer made with number symbols) \n(Example: 17)")
-    creature["initative"] = int(input(""))
+    creature.append(int(input("")))
     print("Is there any notes you would like to add? (Write plain text or leave empty) \n(Example: Will fly in 5 turns. Attacks in one turn.)")
-    creature["notes"] = input("")
-    if f"{name}.txt" in files:
-        with open(f"{name}.txt", "r") as file:
+    creature.append(input(""))
+    if f"{username}.txt" in files:
+        with open(f"{username}.txt", "r") as file:
             data = file.read()
         print(data)
         exec(data)
         print(creature_list)
         creature_list.append(creature)
-        with open(f"{name}.txt", "w") as file:
+        with open(f"{username}.txt", "w") as file:
             file.write(f"creature_list = {creature_list}")
     else:
-        with open(f"{name}.txt", "w") as file:
+        print("path 2")
+        with open(f"{username}.txt", "w") as file:
             file.write(f"creature_list = [{creature}]")
     print("Done!")
         
@@ -52,11 +53,11 @@ def e_use():
     print("")
 
 #Start of code:
-files = os.listdir('Encounters')
-name = "Username####"
-# ---Grab the discord user's ID and save it to the variable "name"---
+files = os.listdir('People')
+files.remove("null..txt")
+username = "Username####"
+# ---Grab the discord user's ID and save it to the variable "username"---
 print("Hello and welcome to the AV D&D Tracker. Start off by picking between managing creatures/characters or encounters. \n(C for Creatures, E for Encounters)")
-print(files)
 choice1 = input("")
 if choice1.lower() == "c":
     print("Next choose between editing an existing creature, copying one that is made, or making a new one.\n(E for Edit, C for Copy, N for New)")
